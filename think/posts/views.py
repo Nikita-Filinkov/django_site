c68e2ae6b9b493dcf844f1aaa2a2bafa8d8c20cc
Nikit <nikita_filinkov@mail.ru>
2025-01-14 21:19:18 +0300

    created new branch

--- think/posts/views.py
+++ think/posts/views.py
@@ -8,4 +8,15 @@ def index(request):
 
 
 def posts(request):
-    return render(request, 'posts/posts.html', {})
+    all_posts = {1: {'title': 'Здание из будущего',
+                     'image': 'Снимок экрана 2023-12-20 194130.png',
+                     'description': '''Описание варианта развития будующего
+                                        в котором возможно вознкновения подобных строений'''},
+                 2: {'title': 'Преодоление',
+                     'image': 'Снимок экрана 2023-10-17 223729.png',
+                     'description': 'Если идти до конца, можно до него дойти'},
+                 3: {'title': 'Прост прикольно)',
+                     'image': '00009-813486580.png',
+                     'description': 'Без комментариев'}
+                 }
+    return render(request, 'posts/posts.html', {'posts': all_posts})
