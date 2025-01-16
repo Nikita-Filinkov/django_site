a0920dd5e7bbbd8cdd74fe39aa55c15f04eeb94a
Nikit <nikita_filinkov@mail.ru>
2025-01-16 22:31:15 +0300

    add model Posts

--- think/posts/models.py
+++ think/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Posts(models.Model):
+    id = models.IntegerField(verbose_name="Id создателя поста")
+    title = models.CharField(max_length=200, verbose_name="Заголовок поста")
+    description = models.TextField(blank=True, verbose_name="Текст поста")
+    images = models.ImageField(verbose_name="Картинки")
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    is_published = models.BooleanField(default=True)
+
