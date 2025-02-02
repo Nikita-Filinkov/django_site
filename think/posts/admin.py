4fdc6188a5b2ec9fd3372fd0513d88f8ab89b30f
Nikit <nikita_filinkov@mail.ru>
2025-02-02 22:41:24 +0300

    add functions for publish

--- think/posts/admin.py
+++ think/posts/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import Count
 
 from .models import Posts, Category, TagPost
@@ -11,15 +11,33 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'title', 'time_created', 'is_published', 'category', 'brief_info')
     list_display_links = ('user_id', 'title')
     ordering = ['-time_created']
-    list_editable = ('is_published',)
+    list_editable = ('is_published', 'category')
     list_per_page = 5
+    actions = ['set_published', 'set_draft']
 
     @admin.display(description="Количество тегов")
     def brief_info(self, posts: Posts):
-        # count_tags = posts.objects.aggregate(Count('tags'))
         tags = posts.tags.aggregate(count_tags=Count('tag'))
-        print(tags)
         return f"Количество тегов у поста: {tags['count_tags']}."
+
+    @admin.action(description='Опубликовать пост')
+    def set_published(self, request, queryset):
+        count_posts = queryset.update(is_published=Posts.Status.PUBLISHED)
+        if count_posts > 1:
+            message = f'Опубликовано {count_posts} поста'
+        else:
+            message = f'Опубликован {count_posts} пост'
+        self.message_user(request, message)
+
+    @admin.action(description='Снять пост с публикации')
+    def set_draft(self, request, queryset):
+        count_posts = queryset.update(is_published=Posts.Status.DRAFT)
+        if count_posts > 1:
+            message = f'Снято с публикации {count_posts} поста'
+        else:
+            message = f'Снят с публикации {count_posts} пост'
+        self.message_user(request, message, messages.WARNING)
+
 # admin.site.register(Posts)
 
 
